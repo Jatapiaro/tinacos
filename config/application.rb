9a14e971e6ad76a2d236c3ae4c770e99c5eb2d46
jacobotapia <jacob.et.cetera@gmail.com>
2017-05-08 19:52:44 -0500

    Lol

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Tinaco
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    #config.time_zone = 'Central Time (US & Canada)'
-    config.active_record.default_timezone = :utc
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
